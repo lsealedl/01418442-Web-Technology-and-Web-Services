5a9b1c2873acb779df1088815087aab5f8fec8a7
Sealed <korn2400@gmail.com>
2024-02-12 11:54:11 +0700

    update

--- New folder (3)/script.js
+++ New folder (3)/script.js
@@ -6,18 +6,27 @@ const getCountry = function (country) {
 	console.log(country);
 	const req = new XMLHttpRequest();
 	req.open("GET", `https://restcountries.com/v3.1/name/${country}`);
+	req.send();
 
-	const html = `
-    <article class="country">
-        <img src="th.png" alt="" class="country_img" />
-        <div class="country_data">
-            <h3 class="country_name">Japan</h3>
-            <h4 class="country_region">Asia</h4>
-        </div>
-    </article>`;
-	countries.insertAdjacentHTML("beforeend", html);
+	req.addEventListener("load", function () {
+		const [data] = JSON.parse(this.responseText);
+		console.log(data);
+		console.log(data.name.common);
+		const html = `
+        <article class="country">
+            <img src="${data.flags.png}" alt="" class="country_img" />
+            <div class="country_data">
+                <h3 class="country_name">${data.name.common}</h3>
+                <h4 class="country_region">${data.region}</h4>
+            </div>
+        </article>`;
+		countries.insertAdjacentHTML("beforeend", html);
+		countries.style.opacity = 1;
+	});
 };
+getCountry("Thailand");
 getCountry("japan");
+getCountry("USA");
 // const emptySpace = document.querySelector(".space");
 
 // const html = `<h2>New Message:</h2><p>this is a message from script</p>`;
