3b8b7ab62207401c5ba62c9cc2759f104ab466f2
Sealed <korn2400@gmail.com>
2024-02-12 11:07:56 +0700

    update

---
+++ New folder (3)/script.js
@@ -0,0 +1,7 @@
+"use strict";
+
+const emptySpace = document.querySelector(".space");
+
+// const html = `<h2>New Message:</h2><p>this is a message from script</p>`;
+
+// emptySpace.insertAdjacentHTML("beforeend", html);
