f6bb3c9217452d68cd919b6404ddc9390c925f2b
Sealed <korn2400@gmail.com>
2024-02-12 11:37:31 +0700

    update

--- New folder (3)/script.js
+++ New folder (3)/script.js
@@ -1,6 +1,24 @@
 "use strict";
 
-const emptySpace = document.querySelector(".space");
+const countries = document.querySelector(".countries");
+
+const getCountry = function (country) {
+	console.log(country);
+	const req = new XMLHttpRequest();
+	req.open("GET", `https://restcountries.com/v3.1/name/${country}`);
+
+	const html = `
+    <article class="country">
+        <img src="th.png" alt="" class="country_img" />
+        <div class="country_data">
+            <h3 class="country_name">Japan</h3>
+            <h4 class="country_region">Asia</h4>
+        </div>
+    </article>`;
+	countries.insertAdjacentHTML("beforeend", html);
+};
+getCountry("japan");
+// const emptySpace = document.querySelector(".space");
 
 // const html = `<h2>New Message:</h2><p>this is a message from script</p>`;
 
